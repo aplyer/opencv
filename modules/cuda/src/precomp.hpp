a2a3eae9797286528a3af6a24d7082ad77049047
aplyer <aurelien@plyer.fr>
2013-10-13 22:00:28 +0200

    modif to compil

--- modules/cuda/src/precomp.hpp
+++ modules/cuda/src/precomp.hpp
@@ -49,6 +49,7 @@
 #include "opencv2/calib3d.hpp"
 #include "opencv2/objdetect.hpp"
 
+#include "opencv2/core/utility.hpp"
 #include "opencv2/core/private.cuda.hpp"
 
 #include "opencv2/opencv_modules.hpp"
